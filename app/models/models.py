53c08b93d25db29c94eb47ff78be2e0c1b4ac9b8
RonJCB <ronellbaptiste30@gmail.com>
2026-04-04 12:04:30 -0400

    Update RoutineExercise and Exercise relationships
    Refactor RoutineExercise and Exercise relationships to use a link model approach.

--- app/models/models.py
+++ app/models/models.py
@@ -2,16 +2,20 @@ from sqlmodel import Field, SQLModel,Relationship
 from typing import Optional
 
 
-
-class UserRoutine(SQLModel, table =True):
+#NO LINK MODEL APPROACH FOR ROUTINEEXERCISE since attributes are added
+class RoutineExercise(SQLModel, table =True):
    routine_id: int = Field(foreign_key="routine.id", primary_key = True)#
    exercise_id: int = Field(foreign_key="exercise.id", primary_key = True)
-
+   sets: int
+   reps: int
+   rest_time: int
+   routine: Optional["Routine"] = Relationship(back_populates="exercises")
+   exercise: Optional["Exercise"] = Relationship(back_populates="routines")
 class Routine(SQLModel, table = True):
     id:int = Field(primary_key = True)
     user_id:int = Field(foreign_key = "user.id")
     user: Optional["User"] = Relationship(back_populates = "routines")
-    exercises:list["Exercise"] = Relationship(back_populates = "routines",link_model = UserRoutine)
+    exercises: list["RoutineExercise"] = Relationship(back_populates="routine")
 
 class Exercise(SQLModel, table = True):
     id:int = Field(primary_key = True)
@@ -24,13 +28,14 @@ class Exercise(SQLModel, table = True):
     equip2:Optional[str] 
     equip3:Optional[str] 
     safety:Optional[str]
-    routines:list["Routine"] =Relationship(back_populates = "exercises",link_model = UserRoutine)
+    routines:list["RoutineExercise"] =Relationship(back_populates = "exercise")
 
 
-    
+  #USES LINK MODEL APPROACH for just the 2 keys  no attributes
 class MealRecipe(SQLModel, table = True):
    recipe_id:int = Field(foreign_key = "recipe.id", primary_key=True)
    meal_id:int = Field(foreign_key = "meal.id", primary_key=True)
+   
 
 class Meal(SQLModel, table = True):
     id:int = Field(primary_key = True)
